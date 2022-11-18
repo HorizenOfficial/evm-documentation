1e5c98c7bd0e419fd8bc729bbfbfa0fad19e8a53
TaiJon Smith <taijonsmith22@gmail.com>
2022-11-18 14:59:13 -0500

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'http://hltoolstest.com/',
+  url: 'http://hltoolstest.com/', 
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
