92f2bc4c38836f7d8a958adffcf7a5b0e6b09c44
TaiJon Smith <taijonsmith22@gmail.com>
2022-11-18 02:23:25 -0500

    removed example docs data

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,10 +34,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -58,7 +54,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'getting-started',
             position: 'left',
             label: 'Tutorial',
           },
