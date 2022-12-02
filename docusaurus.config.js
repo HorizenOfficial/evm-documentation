20b4e021ce2451d06892429d1aaa48018190c937
TaiJon Smith <taijonsmith22@gmail.com>
2022-12-01 21:37:09 -0500

    fixed broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/getting-started',
+                to: '/',
               },
             ],
           },
