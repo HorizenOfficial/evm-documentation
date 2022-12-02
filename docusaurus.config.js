6652e12ca6d7f608f970f268ffa071b9fd45fe36
TaiJon Smith <taijonsmith22@gmail.com>
2022-12-01 21:55:46 -0500

    renamed icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/horizen-menu-icon.svg',
+  favicon: 'img/horizen-icon.svg',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -44,7 +44,7 @@ const config = {
         title: 'Horizen EON Documentation',
         logo: {
           alt: 'Horizen EON Logo',
-          src: 'img/horizen-menu-icon.svg',
+          src: 'img/horizen-icon.svg',
         },
         items: [],
       },
