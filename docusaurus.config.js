87a6cf8b2b64848d28b2839e86f8502251029a31
TaiJon Smith <tsmith@horizenlabs.io>
2023-01-13 14:19:43 -0500

    update url config (#12)
    * update url config
    
    * revert baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Horizen EON Documentation',
-  url: 'http://hltoolstest.com/', 
+  url: 'https://eon.horizen.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
