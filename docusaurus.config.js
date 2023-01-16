44cfe85d12a1057cb00791852950fb6c5431db19
Jofra Montesdeoca <jmontesdeocanuez21@gmail.com>
2023-01-16 17:51:39 +0000

    feat: add Matomo analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,10 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/Horizen_EON_Favicon_256.ico',
 
+  plugins: [
+    "./matomo",
+  ],
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
