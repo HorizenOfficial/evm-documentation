7508229f9ad2fec586ee393a1f0464b35e11d3c5
TaiJon Smith <taijonsmith22@gmail.com>
2023-03-29 18:16:03 -0400

    added algolia info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,6 +60,11 @@ const config = {
           },
         ],
       },
+      algolia: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+      },
       colorMode: {
         defaultMode: 'dark',
       },
