1c4cd50bc88f01749c39e1554d9faaf1f1f695a0
TaiJon Smith <taijonsmith22@gmail.com>
2023-04-03 19:41:42 -0400

    upd fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
         ],
       },
       /* values are defined in cloudflare */
-      algoliaConfig,
+      ...algoliaConfig,
       colorMode: {
         defaultMode: 'dark',
       },
