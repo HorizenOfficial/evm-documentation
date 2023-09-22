7026a346d68d160c1734bcd01c8feb111d988cee
Raby Yuson <ryuson@horizenlabs.io>
2023-09-22 13:47:31 -0400

    keep algolia default search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,10 +24,6 @@ const algoliaConfig = process.env.ALGOLIA_APP_ID
         appId: process.env.ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_API_KEY,
         indexName: process.env.ALGOLIA_INDEX_NAME,
-        replaceSearchResultPathname: {
-          from: '/docs/',
-          to: '/',
-        },
       },
     }
   : {};
