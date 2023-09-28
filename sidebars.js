ee3b942d440468b3be24e4bf35b7f6660517433a
TaiJon Smith <taijonsmith22@gmail.com>
2023-09-28 13:19:54 -0400

    fix autoformat

--- sidebars.js
+++ sidebars.js
@@ -1,200 +1,200 @@
 module.exports = {
   horizen_eon: [
     {
-      type: "category",
-      label: "Introduction - What is EON",
+      type: 'category',
+      label: 'Introduction - What is EON',
       items: [
         {
-          type: "doc",
-          id: "horizen_eon/introduction_what_is_eon/introduction",
+          type: 'doc',
+          id: 'horizen_eon/introduction_what_is_eon/introduction',
         },
         {
-          type: "doc",
-          id: "horizen_eon/introduction_what_is_eon/inside_eon",
+          type: 'doc',
+          id: 'horizen_eon/introduction_what_is_eon/inside_eon',
         },
         {
-          type: "doc",
-          id: "horizen_eon/introduction_what_is_eon/environments",
+          type: 'doc',
+          id: 'horizen_eon/introduction_what_is_eon/environments',
         },
       ],
     },
     {
-      type: "category",
-      label: "Exploring the Network",
+      type: 'category',
+      label: 'Exploring the Network',
       items: [
         {
-          type: "doc",
-          id: "horizen_eon/connect/get_a_wallet",
+          type: 'doc',
+          id: 'horizen_eon/connect/get_a_wallet',
         },
         {
-          type: "doc",
-          id: "horizen_eon/connect/connect_your_wallet",
+          type: 'doc',
+          id: 'horizen_eon/connect/connect_your_wallet',
         },
         {
-          type: "doc",
-          id: "horizen_eon/connect/get_tzen_from_the_faucet",
+          type: 'doc',
+          id: 'horizen_eon/connect/get_tzen_from_the_faucet',
         },
         {
-          type: "doc",
-          id: "horizen_eon/connect/eon_block_explorer",
+          type: 'doc',
+          id: 'horizen_eon/connect/eon_block_explorer',
         },
       ],
     },
     {
-      type: "category",
-      label: "Develop and Deploy - Smart Contracts",
+      type: 'category',
+      label: 'Develop and Deploy - Smart Contracts',
       items: [
         {
-          type: "doc",
-          id: "horizen_eon/develop_and_deploy_smart_contracts/remix",
+          type: 'doc',
+          id: 'horizen_eon/develop_and_deploy_smart_contracts/remix',
         },
         {
-          type: "doc",
-          id: "horizen_eon/develop_and_deploy_smart_contracts/hardhat",
+          type: 'doc',
+          id: 'horizen_eon/develop_and_deploy_smart_contracts/hardhat',
         },
         {
-          type: "doc",
-          id: "horizen_eon/develop_and_deploy_smart_contracts/truffle",
+          type: 'doc',
+          id: 'horizen_eon/develop_and_deploy_smart_contracts/truffle',
         },
         {
-          type: "doc",
-          id: "horizen_eon/develop_and_deploy_smart_contracts/local_build_and_deploy",
+          type: 'doc',
+          id: 'horizen_eon/develop_and_deploy_smart_contracts/local_build_and_deploy',
         },
         {
-          type: "doc",
-          id: "horizen_eon/develop_and_deploy_smart_contracts/verying_a_smart_contract",
+          type: 'doc',
+          id: 'horizen_eon/develop_and_deploy_smart_contracts/verying_a_smart_contract',
         },
       ],
     },
     {
-      type: "category",
-      label: "Staking",
+      type: 'category',
+      label: 'Staking',
       items: [
         {
-          type: "doc",
+          type: 'doc',
           id: "horizen_eon/staking/what_is_staking",
         },
         {
-          type: "doc",
-          id: "horizen_eon/staking/run_a_forger_node",
+          type: 'doc',
+          id: 'horizen_eon/staking/run_a_forger_node',
         },
         {
-          type: "doc",
-          id: "horizen_eon/staking/how_to_stake_on_eon",
-        },
-      ],
-    },
-    {
-      type: "category",
-      label: "Tutorials",
-      items: [
-        {
-          type: "doc",
-          id: "horizen_eon/tutorials/thirdweb",
-        },
-        {
-          type: "doc",
-          id: "horizen_eon/tutorials/todolist",
+          type: 'doc',
+          id: 'horizen_eon/staking/how_to_stake_on_eon',
         },
       ],
     },
     {
-      type: "category",
-      label: "Get Help",
+      type: 'category',
+      label: 'Tutorials',
       items: [
         {
-          type: "doc",
-          id: "horizen_eon/get_help/terminology",
+          type: 'doc',
+          id: 'horizen_eon/tutorials/thirdweb',
         },
         {
-          type: "doc",
-          id: "horizen_eon/get_help/troubleshooting",
-        },
-        {
-          type: "doc",
-          id: "horizen_eon/get_help/disclaimer",
+          type: 'doc',
+          id: 'horizen_eon/tutorials/todolist',
         },
       ],
     },
     {
-      type: "category",
-      label: "Security Guides",
+      type: 'category',
+      label: 'Get Help',
       items: [
         {
-          type: "doc",
-          id: "horizen_eon/security_guides/security_user",
+          type: 'doc',
+          id: 'horizen_eon/get_help/terminology',
+        },
+        {
+          type: 'doc',
+          id: 'horizen_eon/get_help/troubleshooting',
+        },
+        {
+          type: 'doc',
+          id: 'horizen_eon/get_help/disclaimer',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Security Guides',
+      items: [
+        {
+          type: 'doc',
+          id: 'horizen_eon/security_guides/security_user',
         },
       ],
     },
   ],
   governance: [
     {
-      type: "category",
-      label: "Overview",
+      type: 'category',
+      label: 'Overview',
       items: [
         {
-          type: "doc",
-          id: "governance/overview/about",
+          type: 'doc',
+          id: 'governance/overview/about',
         },
         {
-          type: "doc",
-          id: "governance/overview/guiding_values",
+          type: 'doc',
+          id: 'governance/overview/guiding_values',
         },
         {
-          type: "doc",
-          id: "governance/overview/constitution",
+          type: 'doc',
+          id: 'governance/overview/constitution',
         },
         {
-          type: "doc",
-          id: "governance/overview/communication_channels",
+          type: 'doc',
+          id: 'governance/overview/communication_channels',
         },
       ],
     },
     {
-      type: "category",
-      label: "Proposal Process",
+      type: 'category',
+      label: 'Proposal Process',
       items: [
         {
-          type: "doc",
-          id: "governance/proposal_process/proposal_categories",
+          type: 'doc',
+          id: 'governance/proposal_process/proposal_categories',
         },
         {
-          type: "doc",
-          id: "governance/proposal_process/proposal_phases",
+          type: 'doc',
+          id: 'governance/proposal_process/proposal_phases',
         },
       ],
     },
     {
-      type: "category",
-      label: "Voting Process",
+      type: 'category',
+      label: 'Voting Process',
       items: [
         {
-          type: "doc",
-          id: "governance/voting_process/linking_mainchain_address",
+          type: 'doc',
+          id: 'governance/voting_process/linking_mainchain_address',
         },
         {
-          type: "doc",
-          id: "governance/voting_process/voting_process",
+          type: 'doc',
+          id: 'governance/voting_process/voting_process',
         },
       ],
     },
     {
-      type: "category",
-      label: "Reference",
+      type: 'category',
+      label: 'Reference',
       items: [
         {
-          type: "doc",
-          id: "governance/reference/foundational_docs",
+          type: 'doc',
+          id: 'governance/reference/foundational_docs',
         },
         {
-          type: "doc",
-          id: "governance/reference/submit_vote",
+          type: 'doc',
+          id: 'governance/reference/submit_vote',
         },
         {
-          type: "doc",
-          id: "governance/reference/join_discussion",
+          type: 'doc',
+          id: 'governance/reference/join_discussion',
         },
       ],
     },
-  ],
+  ]
 };
