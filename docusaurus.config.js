6a98eedad33f2989eace6b3ea6da5490c6993427
Santi <santi.gamo@gmail.com>
2024-06-07 16:55:56 +0200

    feat: add google analytics (#138)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,6 +66,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-61R2LZVGSH",
+          anonymizeIP: true,
+        }
       }),
     ],
   ],
